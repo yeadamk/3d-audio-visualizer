bebd928b18d9105bfcaee45967a55ed230086f15
Yeadam Kim <yeadamkk@gmail.com>
2025-06-06 05:08:53 -0700

    Add custom shaders

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,10 +1,22 @@
+// ─────────────────────────────────────────────────────────────────────────────
+// fragment.glsl
+// ─────────────────────────────────────────────────────────────────────────────
+precision mediump float;
+
 varying vec3 vNormal;
+
+uniform float u_frequency;
 uniform float u_red;
 uniform float u_green;
 uniform float u_blue;
 
 void main() {
-  float intensity = pow(0.9 - dot(vNormal, vec3(0.0, 0.0, 1.0)), 4.0);
-  vec3 baseColor = vec3(u_red, u_green, u_blue);
-  gl_FragColor = vec4(baseColor * intensity, 1.0);
+  float rim = 1.0 - dot(normalize(vNormal), vec3(0.0, 0.0, 1.0));
+  rim = pow(rim, 2.0);
+
+  vec3 rimColor = vec3(u_red, u_green, u_blue) * rim;
+  vec3 baseAmbient = vec3(0.05);
+  vec3 color = rimColor + baseAmbient;
+
+  gl_FragColor = vec4(color, 1.0);
 }
